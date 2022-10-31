3c3b85b8091c5986a1eb0b327683558c9dc62412
yovana888 <yovana.otaku@gmail.com>
2022-10-30 20:26:49 -0500

    append child

---
+++ selectores/data.js
@@ -0,0 +1,44 @@
+export const data = [
+  {
+    id: "1",
+    author: "Alejandro Escamilla",
+    url: "https://unsplash.com/photos/LNRyGwIJr5c",
+    download_url: "https://picsum.photos/id/1//500/300",
+    description:"Image 1 Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+  },
+  {
+    id: "2",
+    author: "Paul Jarvis",
+    url: "https://unsplash.com/photos/6J--NXulQCs",
+    download_url: "https://picsum.photos/id/10/500/300",
+    description:"Image 2 Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+  },
+  {
+    id: "3",
+    author: "Tina Rataj",
+    url: "https://unsplash.com/photos/pwaaqfoMibI",
+    download_url: "https://picsum.photos/id/100/500/300",
+    description:"Image 3 Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+  },
+  {
+    id: "4",
+    author: "Lukas Budimaier",
+    url: "https://unsplash.com/photos/6cY-FvMlmkQ",
+    download_url: "https://picsum.photos/id/1000/500/300",
+    description:"Image 4 Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+  },
+  {
+    id: "5",
+    author: "Danielle MacInnes",
+    url: "https://unsplash.com/photos/1DkWWN1dr-s",
+    download_url: "https://picsum.photos/id/1001/500/300",
+    description:"Image 5 Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+  },
+  {
+    id: "6",
+    author: "E+N Photographies",
+    url: "https://unsplash.com/photos/GYumuBnTqKc",
+    download_url: "https://picsum.photos/id/1003/500/300",
+    description:"Image 6 Lorem Ipsum is simply dummy text of the printing and typesetting industry."
+  },
+];
--- selectores/selectores.js
+++ selectores/selectores.js
@@ -1,12 +1,39 @@
-const readInput  = () => {
-    const inputs = document.querySelectorAll('.form-control');
-    inputs.forEach(input => {
-        input.style.cssText= `
-        color:blue;
-        border:1px solid purple;
-      `;
-    })
+import {data}  from './data.js';
+
+const showData  = (dataCard) => {
+  const cardElement = document.createElement('div');
+  cardElement.className = "cards_item";
+  
+  const templateCard =
+  `<div class="card" id="${dataCard.id}">
+    <div class="card_image"><img src="${dataCard.download_url}"></div>
+    <div class="card_content">
+    <h2 class="card_title">${dataCard.author}</h2>
+      <p class="card_text">${dataCard.description}</p>
+      <button class="btn card_btn">Read More</button>
+    </div>
+  </div>`;
+ 
+  cardElement.innerHTML = templateCard;
+
+  cardElement.addEventListener('click', () => {
+    let id = cardElement.firstChild.id;
+    showMessage(id);
+  })
+  return cardElement;
+
+}
+
+function showMessage(id){
+  alert(`Su Id es: ${id}` )
+}
+
+const init  = () => {
+    const containerCards = document.querySelector('.cards');
+    for (let key in data) {
+      containerCards.appendChild(showData(data[key]));
+    } 
 }
 
 
-document.addEventListener('DOMContentLoaded', readInput())
+document.addEventListener('DOMContentLoaded', init())
